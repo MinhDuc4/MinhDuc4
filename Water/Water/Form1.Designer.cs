194952a675c3b0d6371328d39aebda3cababfc6e
MinhDuc4 <minhduc812004@gmail.com>
2024-08-07 00:56:25 +0700

    Add files via upload

---
+++ Water/Water/Form1.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace Water
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button3 = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            button4 = new Button();
+            button5 = new Button();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            label7 = new Label();
+            comboBox1 = new ComboBox();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            groupBox1 = new GroupBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            listView1 = new ListView();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(170, 45);
+            button1.Name = "button1";
+            button1.Size = new Size(198, 34);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(170, 111);
+            button2.Name = "button2";
+            button2.Size = new Size(198, 34);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 25);
+            label1.TabIndex = 2;
+            label1.Text = "ID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 116);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 184);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Phone";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(170, 179);
+            button3.Name = "button3";
+            button3.Size = new Size(198, 34);
+            button3.TabIndex = 5;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(587, 50);
+            label4.Name = "label4";
+            label4.Size = new Size(101, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Last Month";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(763, 50);
+            label5.Name = "label5";
+            label5.Size = new Size(101, 25);
+            label5.TabIndex = 7;
+            label5.Text = "This Month";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(587, 88);
+            button4.Name = "button4";
+            button4.Size = new Size(133, 34);
+            button4.TabIndex = 9;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(763, 88);
+            button5.Name = "button5";
+            button5.Size = new Size(133, 34);
+            button5.TabIndex = 10;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(1310, 50);
+            label6.Name = "label6";
+            label6.Size = new Size(97, 25);
+            label6.TabIndex = 11;
+            label6.Text = "Discription";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(1163, 90);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(407, 240);
+            textBox1.TabIndex = 12;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(26, 242);
+            label7.Name = "label7";
+            label7.Size = new Size(86, 54);
+            label7.TabIndex = 13;
+            label7.Text = "Type Of \r\nCustomer";
+            label7.UseCompatibleTextRendering = true;
+            label7.UseWaitCursor = true;
+            label7.Visible = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(170, 255);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(198, 33);
+            comboBox1.TabIndex = 14;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(26, 331);
+            label8.Name = "label8";
+            label8.Size = new Size(107, 50);
+            label8.TabIndex = 15;
+            label8.Text = "Number Of \r\nMember";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(600, 142);
+            label9.Name = "label9";
+            label9.Size = new Size(180, 25);
+            label9.TabIndex = 17;
+            label9.Text = "Amout of water used";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(799, 181);
+            label10.Name = "label10";
+            label10.Size = new Size(38, 25);
+            label10.TabIndex = 19;
+            label10.Text = "m3";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(600, 242);
+            label11.Name = "label11";
+            label11.Size = new Size(169, 25);
+            label11.TabIndex = 21;
+            label11.Text = "Total price (with Tax)";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(799, 299);
+            label12.Name = "label12";
+            label12.Size = new Size(49, 25);
+            label12.TabIndex = 22;
+            label12.Text = "VNĐ";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button10);
+            groupBox1.Controls.Add(button9);
+            groupBox1.Controls.Add(button8);
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label12);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1732, 485);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Caculate Water Bill";
+            // 
+            // textBox2
+            // 
+            textBox2.AcceptsTab = true;
+            textBox2.AllowDrop = true;
+            textBox2.Location = new Point(170, 341);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(198, 31);
+            textBox2.TabIndex = 23;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(600, 178);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(193, 31);
+            textBox3.TabIndex = 24;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(600, 293);
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new Size(193, 31);
+            textBox4.TabIndex = 25;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(86, 434);
+            button6.Name = "button6";
+            button6.Size = new Size(112, 34);
+            button6.TabIndex = 26;
+            button6.Text = "Add";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(367, 434);
+            button7.Name = "button7";
+            button7.Size = new Size(112, 34);
+            button7.TabIndex = 27;
+            button7.Text = "Edit";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(657, 434);
+            button8.Name = "button8";
+            button8.Size = new Size(112, 34);
+            button8.TabIndex = 28;
+            button8.Text = "Delete";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(926, 434);
+            button9.Name = "button9";
+            button9.Size = new Size(112, 34);
+            button9.TabIndex = 29;
+            button9.Text = "Reset";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(1200, 434);
+            button10.Name = "button10";
+            button10.Size = new Size(112, 34);
+            button10.TabIndex = 30;
+            button10.Text = "Print bill";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(12, 503);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(1732, 266);
+            listView1.TabIndex = 11;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1756, 781);
+            Controls.Add(listView1);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button3;
+        private Label label4;
+        private Label label5;
+        private Button button4;
+        private Button button5;
+        private Label label6;
+        private TextBox textBox1;
+        private Label label7;
+        private ComboBox comboBox1;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private GroupBox groupBox1;
+        private Button button10;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private ListView listView1;
+    }
+}
